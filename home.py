15e73a121bd7a2c2d5d2375570e62683e594999c
Fakhita123* <farhan.mustapha2016@gmail.com>
2024-10-06 16:38:25 +0100

    center card

--- home.py
+++ home.py
@@ -8,14 +8,14 @@ def main(page:Page):
     page.scroll="auto"
 
 # creer des livres pour manipulation des donners 
-    l1=Livre("livre 1","harry putter","imagination","oui")
-    l2=Livre("livre 2","no3man","imagination","non")
-    l3=Livre("livre 3","jon sina","imagination","non")
-    l4=Livre("livre 4","spider man","imagination","oui")
-    l5=Livre("livre 5","my life","imagination","oui")
-    l6=Livre("livre 6","dev","imagination","non")
-    l7=Livre("livre 7","harry putter","imagination","non")
-    l8=Livre("livre 8","alten","imagination","oui")
+    l1=Livre("harry putter","imagination","oui")
+    l2=Livre("no3man","imagination","non")
+    l3=Livre("jon sina","imagination","non")
+    l4=Livre("spider man","imagination","oui")
+    l5=Livre("my life","imagination","oui")
+    l6=Livre("dev","imagination","non")
+    l7=Livre("harry putter","imagination","non")
+    l8=Livre("alten","imagination","oui")
 
     list_of_livre=[l1,l2,l3,l4,l5,l6,l7,l8]
 
@@ -38,15 +38,17 @@ def main(page:Page):
         livre_container=Container(
         content=Column(
             controls=[
-                Text(livre.tof),#doit etre recupere de la classe
-                Text(livre.titre),#doit etre recupere de la classe
+                Text(f" Titre : {livre.titre}"),#doit etre recupere de la classe
                 #button peux ouvrir une autre page pour voir la description ou un extrait du livre 
                 ElevatedButton("description",on_click=...),
-                Text(livre.is_disponible)#doit etre recupere de la classe
+                Text(f" disponibilite : {livre.is_disponible}")#doit etre recupere de la classe
             ]
         ),
         bgcolor=colors.AMBER_300,
         width=250,
+        padding=20,
+        border_radius=10,
+        alignment=alignment.center
        )
         livre_containers.append(livre_container)
     
--- livre.py
+++ livre.py
@@ -1,6 +1,9 @@
 class Livre():
-    def __init__(self,tof,titre,description,is_disponible):
-        self.tof=tof
+    def __init__(self,titre,description,is_disponible):
         self.titre=titre
         self.description=description
         self.is_disponible=is_disponible
+
+    
+    def ajouter_livre():
+        pass
